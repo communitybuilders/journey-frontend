43e81ee47ab77c8a7122c300212430498b594c6a
Joseph Martin <joseph.martin@legalconsult.com.au>
2016-05-28 20:36:49 +1000

    Attempting to get chart working

--- js/cards.js
+++ js/cards.js
@@ -1,5 +1,3 @@
-
-
 $(document).ready(function() {
     card_generator();
 });
--- js/chart.js
+++ js/chart.js
@@ -1,11 +1,37 @@
+$(document).ready(function() {
+    chart_values();
+});
+
+function chart_values(){
+//    $.ajax({
+//        url: "https://journey.donorwallet.com.au/api/rest",
+//        method: "POST",
+//        data: {
+//            action_type: "summary_statistic",
+//            api_key: "<api key>",
+//            entity_type: 'campaign',
+//            entity_id: '787',
+//            campaign_identifier: 'Jodie'
+//        },
+//        dataType: 'jsonp',
+//        jsonpCallback: 'callback',
+//        contentType: "application/json"
+//    }).then(function(data) {
+//        console.log(data);
+//    });
+}
+
+
+var number = randomNumber = Math.floor(Math.random()*(100-10+1)+10);
+var number2 = 100 - number;
 
 var pieData = [
     {
-        value : 30,
+        value : number,
         color : "#fb8c00"
     },
     {
-        value : 70,
+        value : number2,
         color : "#eee"
     }
 ];
