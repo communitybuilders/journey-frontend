59da8e5b5e961e7c9db16f86b1522c3a984278a3
Dejan <dejan@legalconsult.com.au>
2016-05-30 11:09:21 +1000

    Updated JavaScript to correctly handle Cross-Origin AJAX call using JSONP.

--- js/chart.js
+++ js/chart.js
@@ -3,38 +3,45 @@ $(document).ready(function() {
 });
 
 function chart_values(){
-//    $.ajax({
-//        url: "https://journey.donorwallet.com.au/api/rest",
-//        method: "POST",
-//        data: {
-//            action_type: "summary_statistic",
-//            api_key: "<api key>",
-//            entity_type: 'campaign',
-//            entity_id: '787',
-//            campaign_identifier: 'Jodie'
-//        },
-//        dataType: 'jsonp',
-//        jsonpCallback: 'callback',
-//        contentType: "application/json"
-//    }).then(function(data) {
-//        console.log(data);
-//    });
+   $.ajax({
+       url: "https://journey.donorwallet.com.au/api/rest",
+       method: "POST",
+       data: {
+           action_type: "summary_statistic",
+           api_key: "{{API_KEY}}",
+           entity_type: 'campaign',
+           entity_id: '787',
+           campaign_identifier: 'Jodie',
+           include_target_data: "true"
+       },
+       dataType: 'jsonp',
+       jsonpCallback: "jsonpCallback",
+       contentType: "application/json;charset=utf-8"
+   });
 }
 
+function jsonpCallback(data) {
+    var success = data.operation !== undefined && data.operation !== "failed" && data.result !== undefined;
 
-var number = randomNumber = Math.floor(Math.random()*(100-10+1)+10);
-var number2 = 100 - number;
+    if( success ) {
+        // Success!
+        if( data.result.targets !== undefined && data.result.targets.length > 0 ) {
+            var target = data.result.targets[0];
 
-var pieData = [
-    {
-        value : number,
-        color : "#fb8c00"
-    },
-    {
-        value : number2,
-        color : "#eee"
+            $("#amount_raised").text("$ " + target.current_value);
+
+            var pieData = [
+                {
+                    value : target.current_value,
+                    color : "#fb8c00"
+                },
+                {
+                    value : target.target_value,
+                    color : "#eee"
+                }
+            ];
+
+            new Chart(document.getElementById("graph").getContext("2d")).Doughnut(pieData, {percentageInnerCutout : 62});
+        }
     }
-];
-
-new Chart(document.getElementById("graph").getContext("2d")).Doughnut(pieData,{percentageInnerCutout : 62});
-
+}
