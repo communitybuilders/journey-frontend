c1bbf10b8cbcb54ffec8a8ade2bfced2e7b17051
Joseph Martin <joseph.martin@legalconsult.com.au>
2016-09-26 11:06:38 +1000

    Updating chart to use backend

--- js/profile.js
+++ js/profile.js
@@ -27,6 +27,37 @@ function profile(){
         $('#verified_by').append(data.title[0].value);
         $('.donate_button').append('<a href="' + data.field_donate_url[0].uri + '" class="btn-large waves-effect waves-light orange darken-1">Donate</a>');
 
+        if (data.field_dw_campaign_id[0].value) {
+            chart_values(data.field_dw_campaign_id[0].value);
+        }
 
+    });
+}
+
+function chart_values(entity_id){
+
+    var url = "https://journey.communitybuilders.com.au/donorwallet?entity_id=" + entity_id;
+
+    $.get(url, function(data) {
+        var result = $.parseJSON(data);
+        // Success!
+        if( result.operation == "success" ) {
+            var target = result.result.targets[0];
+
+            $("#amount_raised").text("$ " + target.current_value);
+
+            var pieData = [
+                {
+                    value : target.current_value,
+                    color : "#fb8c00"
+                },
+                {
+                    value : target.target_value,
+                    color : "#eee"
+                }
+            ];
+
+            new Chart(document.getElementById("graph").getContext("2d")).Doughnut(pieData, {percentageInnerCutout : 62});
+        }
     });
 }
